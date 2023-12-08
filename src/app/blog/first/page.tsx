f1ab7d5dd0e15d875b89c9d08915da004389a892
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-12-09 00:31:07 +0300

    nested routing

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,8 @@
+export default function First() {
+  return (
+    <div>
+      <h1>First</h1>
+      <p>My First</p>
+    </div>
+  );
+}
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,8 @@
+export default function Second() {
+    return (
+        <div>
+        <h1>Second</h1>
+        <p>My second</p>
+        </div>
+    );
+}
