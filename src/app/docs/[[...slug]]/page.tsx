870709500d447253daa202a47d56ca7567a5c8be
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-12-09 00:52:47 +0300

    catch all route

---
+++ src/app/docs/[[...slug]]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DocsHomepage = ({
+  params,
+}: {
+  params: {
+    slug: string[];
+  };
+}) => {
+  return <div>Docs Homepage</div>;
+};
+
+export default DocsHomepage;
