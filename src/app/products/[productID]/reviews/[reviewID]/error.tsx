d71bb6f32b4832dedea558b92b0da974c393d8e3
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-01-07 16:33:34 +0300

    error handling

---
+++ src/app/products/[productID]/reviews/[reviewID]/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const Error = ({ error }: {
+  error: Error
+}) => {
+  return <div>Error: {error.message}</div>;
+};
+
+export default Error;
--- src/app/products/[productID]/reviews/[reviewID]/page.tsx
+++ src/app/products/[productID]/reviews/[reviewID]/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+function randomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
 const Review = ({
   params,
 }: {
@@ -8,6 +12,12 @@ const Review = ({
     reviewID: string;
   };
 }) => {
+  const random = randomInt(2);
+
+  if (random === 1) {
+    throw new Error("Error loading review");
+  }
+
   return (
     <div>
       Review {params.reviewID} for product {params.productID}
